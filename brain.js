d5adbad1d3aa522c449f34b56dec4d7d3e751a35
Emmanuel uma <umaemmanuel62@gmail.com>
2023-02-01 16:47:07 +0100

    working of the contact page

--- brain.js
+++ brain.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+const menuBtn = document.getElementById('menuBtn');
+const stuff = document.getElementById('stuff')
+
+menuBtn.addEventListener('click', e => {
+    if(menuBtn.classList.contains('fa-bars')){
+        menuBtn.style.transform = 'rotate(0deg)';
+        menuBtn.style.transform = 'rotate(180deg)';
+        menuBtn.classList.remove('fa-bars');
+        menuBtn.classList.add('fa-times');
+        stuff.classList.toggle('active');
+
+    }else if (menuBtn.classList.contains('fa-times')){
+        menuBtn.style.transform = 'rotate(360deg)';
+        menuBtn.classList.remove('fa-times');
+        menuBtn.classList.add('fa-bars');
+        stuff.classList.toggle('active');
+    }
+})
