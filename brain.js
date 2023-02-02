e2b534ac5087102e126a2bfba76bab4a2a36e45a
Emmanuel uma <umaemmanuel62@gmail.com>
2023-02-02 09:01:30 +0100

    done with the email api, now the validation

--- brain.js
+++ brain.js
@@ -16,3 +16,41 @@ menuBtn.addEventListener('click', e => {
         stuff.classList.toggle('active');
     }
 })
+
+
+const btnS =document.getElementById('btnS');
+const form =  document.getElementById('form')
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+})
+
+btnS.addEventListener('click', e =>{
+    const name1 = document.getElementById('name');
+    const email = document.getElementById('email');
+    const subject = document.getElementById('subject');
+    const message = document.getElementById('message');
+    const body1 = 
+        `Name: ${name1.value} </br>
+        Email address: ${email.value} </br>
+        Subject: ${subject.value} </br>
+        message: ${message.value}
+        `;
+        console.log(name1.value,email.value,subject.value,message.value)
+    e.preventDefault();
+    Email.send({
+        SecureToken : "d3782a77-1d2c-4f12-8850-c4913226cb27",
+        To : "umaemmanuel62@gmail.com",
+        From : email.value,
+        Subject : subject.value,
+        Body : body1
+    }).then(
+      message => alert(message)
+    ).catch(data => {
+        alert('error')
+    });
+})
+
+const f = '5A786BAF0F2606A3F2469D5746BD39659180';
+const Password = ' d3782a77-1d2c-4f12-8850-c4913226cb27'
