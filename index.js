9d4abef10b72b1ad68824dc0fe32545c4985761d
Emmanuel uma <umaemmanuel62@gmail.com>
2023-03-17 16:14:48 +0100

    added the api

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-/* const youtube = document.querySelector('.youtube-container');
+const youtubeApi = document.querySelector('.youtubeApi-container');
+console.log(youtubeApi)
 const v = 'https://www.youtube.com/watch?v=';
-console.log(youtube)
 const key = 'AIzaSyDItEu4dUQISg77wPjpHhMyVFvsQsnuFJs';
-fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UComu3ao5983Wpni7R_JcovQ&maxResults=9&order=date&key=${key}`).then(
+fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UComu3ao5983Wpni7R_JcovQ&maxResults=4&order=date&key=${key}`).then(
     data => {
         return data.json();
     }
@@ -10,11 +10,13 @@ fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=U
 .then(res => {
     const videos = res.items;
     for(video of videos) {
-        youtube.innerHTML += `
+        youtubeApi.innerHTML += `
         <div class="grid-item">
             <a href="${v}${video.id.videoId}" target="_blank" rel="noopener noreferrer">
                 <img src="${video.snippet.thumbnails.high.url}">
-                <div class="overlay"></div>
+                <div class="overlay">
+                    <p>follow@thecritics001 <i class="fa-brands fa-youtube"></i></p>
+                </div>
             </a>
         </div>
         `
@@ -23,4 +25,3 @@ fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=U
 }).catch(err =>{
     console.log(err)
 })
- */
